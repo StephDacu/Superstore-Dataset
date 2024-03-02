858ed5d18985da967621ba140a263f26d6028a8f
Stephen Dacuyan <161432984+StephDacu@users.noreply.github.com>
2024-03-01 18:56:26 -0800

    Add files via upload

---
+++ create_superstore_table.sql
@@ -0,0 +1,16 @@
+-- Creating a table for the superstore dataset
+CREATE TABLE superstore (
+	ship_mode VARCHAR(100),
+	segment VARCHAR(50),
+	country VARCHAR(50),
+	city VARCHAR(50),
+	state VARCHAR(50),
+	postal_code INT,
+	region VARCHAR(50),
+	category VARCHAR(50),
+	sub_category VARCHAR (50),
+	sales DEC,
+	quantity INT,
+	discount DEC,
+	profit DEC
+);
